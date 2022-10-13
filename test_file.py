eecff16dba2fd68a94cdf46258fb843cf4594771
Patrik Holub <23bulohp@gmail.com>
2022-10-13 19:25:12 +0200

    Going with JSON, less work on pico

--- test_file.py
+++ test_file.py
@@ -1,6 +1,4 @@
 import json
-import yaml
-from yaml.loader import SafeLoader
 
 #This is the first general file
 class player:
@@ -19,12 +17,4 @@ Giganto = player(player_data["player info"][0])
 
 f.close()
 
-
 print(Giganto.HP)
-
-player_file = open("player.yml", "r")
-player_yaml_data = yaml.load(player_file, Loader=SafeLoader)
-
-Magnifico = player(player_yaml_data['player_info'])
-
-print(Magnifico.HP)
