2be55ecaef2a8f608f9acaad18e978ba55f02ef7
Patrik Holub <23bulohp@gmail.com>
2022-10-14 20:15:10 +0200

    More or less working

--- python/test_file.py
+++ python/test_file.py
@@ -4,39 +4,68 @@ import serial
 from time import sleep
 
 #global variables
-terminator = "\r"
+terminator = "\r\n"
+
+comm = serial.Serial('COM3', 112500, timeout=0.01)
+last_send = ''
 
 
-#This is the first general file
+# This is the first general file
 class player:
-    
-    def __init__(self, PlayerData):
-        self.HP = PlayerData['HP']
-        self.MP = PlayerData['MP']
-        self.Name = PlayerData['Name']
-        self.Age = PlayerData['Age']
-        self.Gender = PlayerData['Gender']
+
+	def __init__(self, PlayerData):
+		self.HP = PlayerData['HP']
+		self.MP = PlayerData['MP']
+		self.Name = PlayerData['Name']
+		self.Age = PlayerData['Age']
+		self.Gender = PlayerData['Gender']
 
 
-#working file code, reuse later, commented for now
+# working file code, reuse later, commented for now
 
 #f = open('player.json',)
 
 #player_data = json.load(f)
 #Giganto = player(player_data["player info"][0])
 
-#f.close()
+# f.close()
 
-#print(Giganto.HP)
+# print(Giganto.HP)
 
-comm = serial.Serial('COM3', 112500, timeout=0.1)
+def all_equal(iterator):
+	iterator = iter(iterator)
+	try:
+		first = next(iterator)
+	except StopIteration:
+		return True
+	return all(first == x for x in iterator)
+
+
+def sendMessage(msg):
+	b_msg = bytes(msg+terminator, 'UTF-8')
+	global last_send
+	last_send = b_msg
+	comm.write(b_msg)
+
+
+def readMessage(decoding=False, encoding='UTF-8'):
+	data = comm.readlines()
+	if (last_send == data[0]):
+		better_data = data[1:]
+	else:
+		better_data = data;
+	if decoding:
+		for line in better_data:
+			line = line.decode(encoding)
+		return better_data
+	else:
+		return data
+
 
 while True:
-    send_data = input()
-    comm.write(bytes(send_data+terminator, 'UTF-8'))
-    sleep(1)
-    received_data = comm.readlines()[1]
-    print(received_data.decode('UTF-8'))
-    new_message = comm.readline()
-    print(new_message)
-    
+	message = input()
+	print(last_send)
+	input()
+	sendMessage(message)
+	print(readMessage(1))
+	
