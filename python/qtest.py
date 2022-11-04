122643f537e9b025a053558211ad6aab5782f08e
Patrik Holub <23bulohp@gmail.com>
2022-11-04 14:43:25 +0100

    progress

--- python/qtest.py
+++ python/qtest.py
@@ -1,4 +1,14 @@
 to_convert = 'HELLO'
 max_len = max([ord(letter) for letter in to_convert])
 
-print(max_len)
+div = 0
+while max_len/pow(10, div) > 1 :
+	div += 1
+
+converted = str(hex(div))
+for letter in to_convert:
+	converted += str(hex(ord(letter))).replace('0x','')
+
+
+
+print(converted)
