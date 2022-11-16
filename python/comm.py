cf9476af0c64d4e6df3c286505597587bb45a2e0
Patrik Holub <113500517+Holub-Patrik@users.noreply.github.com>
2022-11-16 10:39:59 +0100

    Adding comments

--- python/comm.py
+++ python/comm.py
@@ -10,34 +10,52 @@ last_send = ''
 
 def playerID(comport):
 	sendMessage(comport,"ID")
-	id_rcvd = readMessage(comport, True)
-	print(id_rcvd)
-	ind_string = ''
+	id_rcvd = readMessage(comport, True) # send for answer with the id and listen for it
+	print(id_rcvd) # just a simple check output
+	ind_string = '' # empty string to handle the next block
 	if((index_start := id_rcvd.index("[")) > 0 and (index_end := id_rcvd.index("]")) > 0):
-		ind_string = str(id_rcvd[index_start+1:index_end])
+		# "simple" check if both of the brackets are present in ID, otherwise the slice function would fail
+		ind_string = str(id_rcvd[index_start+1:index_end]) # slicing the string
+	return ind_string
+
+def chck_player(comport):
+	sendMessage(comport,"ID")
+	id_rcvd = readMessage(comport, True) # send for answer with the id and listen for it
+	print(id_rcvd) # just a simple check output
+	ind_string = '' # empty string to handle the next block
+	if((index_start := id_rcvd.index("[")) > 0 and (index_end := id_rcvd.index("]")) > 0):
+		# "simple" check if both of the brackets are present in ID, otherwise the slice function would fail
+		ind_string = str(id_rcvd[:index_start]) # slicing the part before the ID itself
 	return ind_string
 
 def findDevices():
-	ports = serial.tools.list_ports.comports()
-	print(ports[0].name)
-	counter = 0
+	ports = serial.tools.list_ports.comports() # list of all the ports present on the machine
+	print(ports[0].name) # just a control line to see in console if reading comports rigt
+	counter = 0 # counter for while function and a tool for assigning the ports to a dict
 	while counter<len(ports):
 		try:
 			temp_comm = serial.Serial(ports[counter].name, 112500, timeout=1)
-			comm.update({counter:temp_comm})
-			player_ID = playerID(counter)
-			if(player_ID == 'player'):
+			# open a serial line so I can communicate with the device, if fails, falls to except
+			comm.update({counter:temp_comm}) # update the dict with a numerical key
+			player_chck = chck_player(counter) # send a message to get the player ID
+			if(player_chck == 'player'):
+				# check if the the ID is really id of player
 				print("Succsesfully added port: " + ports[counter].name)
-				comm.update({playerID:counter})
+				comm.update({playerID(counter):counter})
+				# if nothing errored out, good and add the player id and which numerical index of port it has
 			else:
 				print("Wrong device on port: " + ports[counter].name)
 				comm.popitem()
+				# wrong id came through and player isn't on the other side and delete the comport from the list
 			counter += 1
+			# everything went well so we can move on to the next comport
 		except Exception as e:
 			print("Can't open the port: " + ports[counter].name)
 			print(e)
 			counter += 1
+			# something errored out, print it and continue on the nesxt port
 
+	# the dict has a fucky wucky structure so fix it
 	fix_count = 0
 	key = list(comm.keys())
 	val = list(comm.values())
@@ -45,25 +63,30 @@ def findDevices():
 		if(key[fix_count] is val[fix_count+1]):
 			comm[key[fix_count+1]] = comm[key[fix_count]]
 			del comm[key[fix_count]]
-	print(comm)
+	# I wrote it last week and I have no idea what the fuck is going on here anymore
+	print(comm) # control print
 
 def sendMessage(index,msg):
+	# message has to be formated, thus function to format and send
 	b_msg = bytes(msg + terminator, 'UTF-8')
 	global last_send
 	last_send = b_msg
 	comm[index].write(b_msg)
 	
 def readMessage(index, decoding = False, encoding = 'UTF-8'):
+	# message has to be read and decoded, if you want
+	# !!!!!!!!!!!!!!!!! THIS FUNCTION WILL READ ONE LINE AFTER THE THE FEEDBACK MESSAGE !!!!!!!!!!!!!!!!!
 	data = comm[index].readlines()
 	iterator = iter(data)
 	index = 0
 	while(next(iterator) == last_send):
 		index += 1
-	
+	# used to get the message out of it and not the feedback
 	better_data = data[index:]
-	return (better_data[0].decode('UTF-8')).strip() if decoding else better_data[0]
+	return (better_data[0].decode('UTF-8')).strip() if decoding else better_data[0] # return decoded or encoded, doesn't matter
 
 def readMessageBlock(index,decoding = False, encoding = 'UTF-8'):
+	# The same as readMessage but used to get the entire block of data coming through
 	data = comm[index].readlines()
 	iterator = iter(data)
 	index = 0
