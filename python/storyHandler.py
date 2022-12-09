33732105f4f9142eb5e881bbbf5c47d4fcc56180
Patrik Holub <23bulohp@gmail.com>
2022-12-09 20:33:15 +0100

    don't hate me lord

--- python/storyHandler.py
+++ python/storyHandler.py
@@ -3,22 +3,17 @@ import os
 def createStructure(folder):
     depth = 0
     path = os.path.join(os.getcwd(),folder)
-    folderDepthCounter(path)
+    depth = list_files(path)
     
     print(depth)
             
-def folderDepthCounter(path):
-    curr_path = path
-    curr_depth = 0
-    iterloop(curr_path,0)
-    def iterloop(curr_path, my_number):
-        layer = os.listdir(curr_path)
-        for item in layer:
-            if allFiles(os.listdir(os.path.join(curr_path,item))):
-                return my_number
-            if os.path.isdir(os.path.join(curr_path,item), my_number+1):
-                iterloop(os.path.join(curr_path,item), my_number+1)
-    return depth
+def list_files(startpath):
+    max_level = 0
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        max_level = level if level > max_level else max_level
+        
+    return max_level
 
 def allFiles(layer):
 	iterator = iter(layer)
