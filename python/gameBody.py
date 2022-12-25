e9e7eb150bbbe16f0fa54eeec2d940af2f1ef0cb
Patrik Holub <23bulohp@gmail.com>
2022-12-25 22:22:42 +0100

    How tf do you make the evenstack?????

--- python/gameBody.py
+++ python/gameBody.py
@@ -1,14 +1,30 @@
 import comm
 import deviceHandler
+import json
+import fileHandler
+import os
 
 #TODO Create a game body
 
 #? I need to implement an event stack that advances each round
 #* Create a list of players either through comm or through searching in files
+playerlist = []
+eventStack = []
 
-def getPlayers():
-    devicesList = deviceHandler.findDevices()
-    playerlist = []
-    for player in devicesList:
-        comm.sendMessage(player, 'sendpdata')
-        comm.readMessageBlock(player, True)
+folder = os.path.join(os.getcwd(), 'player')
+
+def getPlayers(method):
+    match method:
+        case 'device':
+            devicesList = deviceHandler.findDevices()
+            for player in devicesList:
+                comm.sendMessage(player, 'sendpdata')
+                playerlist.append(json.loads(comm.readMessageBlock(player, True)))
+        case 'files':
+            players = os.listdir(folder)
+            for player in players:
+                playerlist.append(fileHandler.read(os.path.join(folder, player)))
+
+getPlayers('files')
+print(playerlist)
+print(playerlist[0]['player_info']['Name'])
--- python/players.py
+++ python/players.py
@@ -10,13 +10,53 @@ class Player:
 		self.Name = PlayerData['Name']
 		self.Age = PlayerData['Age']
 		self.Gender = PlayerData['Gender']
+
+	def getVariable(variable):
+		global HP, MP, Name, Age, Gender
+		match variable:
+			case 'HP':
+				return HP
+			case 'MP':
+				return MP
+			case 'Name':
+				return Name
+			case 'Age':
+				return Age
+			case 'Gender':
+				return Gender
+				
+	def setVariable(variable, new_value):
+		global HP, MP, Name, Age, Gender
+		match variable:
+			case 'HP':
+				HP = new_value
+				return 1
+			case 'MP':
+				MP = new_value
+				return 1
+			case 'Name':
+				Name = new_value
+				return 1
+			case 'Age':
+				Age = new_value
+				return 1
+			case 'Gender':
+				Gender = new_value
+				return 1
 		
-f = open('player.json')
+	def attack(player, dmg_amount):
+		player.getHit(dmg_amount)
+	
+	def getHit(dmg_amount):
+		HP-=dmg_amount
+		
+f = open('player/player.json')
 
 player_data = json.load(f)
-Giganto = Player(player_data["player info"][0])
+Giganto = Player(player_data["player_info"])
 
 f.close()
+
 print(Giganto.HP)
 
 #def all_equal(interator):
