6d37ed3fed2d562c99649da27bc172ae47e49cf1
Patrik Holub <23bulohp@gmail.com>
2023-01-06 21:27:06 +0100

    rougue 300 print

--- python/gameBody.py
+++ python/gameBody.py
@@ -20,21 +20,23 @@ storyStack = storyHandler.get_storyparts(story_path)
 
 #* import loop
 scripts_path = fileHandler.read('scriptlocation.json')
-print(scripts_path)
 
 event_scripts_list = []
 
 for index,path in scripts_path.items():
 	import_path = os.path.join(os.getcwd(),path)
-	sys.path.insert(index,import_path)
-	event_scripts_list.append([event.replace('.py','') for event in os.listdir(import_path) if os.path.isfile(os.path.join(import_path, event))])
-
+	sys.path.insert(int(index),import_path)
+	for event in os.listdir(import_path):
+		if os.path.isfile(os.path.join(import_path, event)): 
+			event_scripts_list.append(event.replace('.py',''))
 
 import_list = []
 for event in event_scripts_list:
 	import_list.append(importlib.import_module(event.replace('.py','')))
 
 imports = {keys:values for keys in event_scripts_list for values in import_list}
+
+print(imports) 
 
 def handle(event_string, arguments):
 	function = getattr(imports[event_string],event_string)
@@ -60,15 +62,15 @@ def getPlayers(method):
 #print(playerlist)
 #print(playerlist[0]['player_info']['Name'])
 
-for event in storyStack:
-	npc_list = []
-	txt_to_draw = fileHandler.read(os.path.join(event, 'storypart.txt'))
-	try:
-		npc_path = fileHandler.getFolder(event,'npc')
-		npc_file_list = [file for file in os.listdir(npc_path)]
-		for file in npc_file_list:
-			npc_list.append(fileHandler.read(os.path.join(npc_path,file)))
-	except:
-		print("No NPC found")
-	print(npc_list)
-	#window.send_txt(txt_to_draw)
+#for event in storyStack:
+#	npc_list = []
+#	txt_to_draw = fileHandler.read(os.path.join(event, 'storypart.txt'))
+#	try:
+#		npc_path = fileHandler.getFolder(event,'npc')
+#		npc_file_list = [file for file in os.listdir(npc_path)]
+#		for file in npc_file_list:
+#			npc_list.append(fileHandler.read(os.path.join(npc_path,file)))
+#	except:
+#		print("No NPC found")
+#	print(npc_list)
+#	#window.send_txt(txt_to_draw)
