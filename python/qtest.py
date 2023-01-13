243fa3442c7637d6d46ec23aef5c4ce823c3c35f
Patrik Holub <23bulohp@gmail.com>
2023-01-13 22:15:42 +0100

    IT IS DONE

--- python/qtest.py
+++ python/qtest.py
@@ -18,6 +18,29 @@ class window():
 		pass
 
 	def add_text(self, text):
+		hook_count = 0
+		max_word_len = 0
+		max_break_num = 0
+		for line in text.splitlines():
+			if '<h>' in line:
+				hook_count += 1
+			if '<un>' in line:
+				hook_count = 0
+			for word in line.split(' '):
+				if len(word) > max_word_len:
+					max_word_len = len(word)
+			if ceil(len(line)/self.width) > max_break_num:
+				max_break_num = ceil(len(line)/self.width)
+		
+		if hook_count >= self.height:
+			raise Exception("Too many hooks in text for screen")
+		elif max_word_len > self.width:
+			raise Exception("Words too long for screen")
+		elif max_break_num > self.height:
+			raise Exception("Lines too long for screen")
+		elif self.height - max_break_num < hook_count:
+			raise Exception("Too many hooks in text for screen")
+
 		for line in text.splitlines():
 			self.screen.append(line)
 
@@ -31,22 +54,25 @@ class window():
 
 		line = self.screen[self.start_index]
 		line_break_num = ceil(len(line)/self.width)
-		
-		
+				
 		if self.curr_height <= self.height:
 			self.end_index += 1
 		self.curr_height += line_break_num
-		
+
 		if self.skip == 0:
 			self.render = self.screen[self.start_index:self.end_index]
 		else:
 			self.render = self.render[:self.skip]
 			for line in self.screen[self.start_index+self.skip:self.end_index]:
 				self.render.append(line)
-
-		if self.curr_height >= self.height:
-			self.start_index += 1
-			self.curr_height -= line_break_num
+		if self.height % line_break_num != 0:
+			if self.curr_height + line_break_num >= self.height:
+				self.start_index += 1
+				self.curr_height -= line_break_num
+		else:
+			if self.curr_height >= self.height:
+				self.start_index += 1
+				self.curr_height -= line_break_num
 
 		i = self.skip
 
@@ -93,7 +119,7 @@ class window():
 			
 				
 		
-window = window(10, 10)
+window = window(60, 10)
 text = open('test.txt', 'r').read()
 window.add_text(text)
 while True:
