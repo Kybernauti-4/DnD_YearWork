0389a01bdf83b6d05ec40f9ac2e17c9b5d66468b
D32\zak <spseplze>
2023-01-25 10:38:32 +0100

    fuck me

--- python/script_tool/edit.py
+++ python/script_tool/edit.py
@@ -1,6 +1,26 @@
+def edit(path):
+	print('What am I supposed to edit?')
 
-
+def create(args, path):
+	file_type = args[0]
+	match file_type:
+		case 'json':
+			print('Creating a json file')
+		case 'txt':
+			print('Creating a txt file')
+		case 'sc_info':
+			print('Creating a sc_info file')
+		case 'events':
+			print('Creating an events file')
+		case 'player':
+			print('Creating a player file')
+		case 'player_template':
+			print('Creating a player_template file')
+		case _:
+			print('Invalid file type')
+	
 def get_attr(path):
-	{
+	return {
 		'edit': [[], 'No file to edit'],
+		'create': [['json', 'txt', 'sc_info', 'events', 'player', 'player_template'], 'No file to create'],
 	}
