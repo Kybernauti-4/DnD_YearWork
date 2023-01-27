f979de7d20d1ada8b703479249887a72f34a0e3d
Patrik Holub <23bulohp@gmail.com>
2023-01-27 18:16:29 +0100

    pain

--- python/script_tool/edit.py
+++ python/script_tool/edit.py
@@ -1,4 +1,4 @@
-def edit(path):
+def edit(args, path):
 	print('What am I supposed to edit?')
 
 def create(args, path):
@@ -18,13 +18,31 @@ def create(args, path):
 			print('Creating a player_template file')
 		case _:
 			print('Invalid file type')
+
+def c(args, path):
+	file_type = args[0]
+	match file_type:
+		case 'json':
+			print('Creating a json file')
+		case 'txt':
+			print('Creating a txt file')
+		case 'sc_info':
+			print('Creating a sc_info file')
+		case 'events':
+			print('Creating an events file')
+		case 'player':
+			print('Creating a player file')
+		case 'player_template':
+			print('Creating a player_template file')
+		case _:
+			print('Invalid file type')
 	
 def get_attr(path):
-	create_command = ['create', 'c']
-	create_options = ['json', 'txt', 'sc_info', 'events', 'player', 'player_template']
-	tags = ['-empty', '-e', '-notepad', '-np']
-	for command in create_command:
-		return_dict.update({command: [create_options, '']})
 
-	return_dict = {}
+	return_dict = {
+		'create': [['json', 'txt', 'sc_info', 'events', 'player', 'player_template'],''],
+		'c':[['json', 'txt', 'sc_info', 'events', 'player', 'player_template'],''],
+		'edit': [['path_files'],'No files found'],
+		'e': [['path_files'],'No files found'],
+		}
 	return return_dict
