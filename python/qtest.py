4a12e003dbed7c398ad360c8a086d62f52f585fb
Patrik Holub <23bulohp@gmail.com>
2023-01-29 11:30:47 +0100

    pepepain

--- python/qtest.py
+++ python/qtest.py
@@ -5,12 +5,6 @@ with open(os.path.join(os.path.dirname(__file__), 'test.json'), 'r') as f:
 	file_content = json.load(f)
 
 screen = []
-curr_line = ''
-for char in json.dumps(file_content, indent=4):
-	if char == '\n':
-		screen.append(curr_line)
-		curr_line = ''
-		continue
-	curr_line += char
+
 
 print(screen)
