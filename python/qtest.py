911b595840f208014fc0a248415f2db49509e9be
D32\zak <spseplze>
2023-01-30 10:27:28 +0100

    pepepain

--- python/qtest.py
+++ python/qtest.py
@@ -4,34 +4,55 @@ import keyboard
 
 
 
-def update_variable(e, vertical, screen, line):
-	if e.name == 'up':
-		if vertical[0] > 0:
-			curr_line = screen[vertical[0]]
-			print(f'\r\u001b[0m{curr_line}', end='', flush=True)
-			print(f'\r\u001b[A', end='', flush=True)
+def update_variable(e, vertical, screen, line, context_menu, context, horizontal, cm_idx):
+	if line[0]	== '':
+		if e.name == 'up':
+			if vertical[0] > 0:
+				curr_line = screen[vertical[0]]
+				print(f'\r\u001b[0m{curr_line}', end='', flush=True)
+				print(f'\r\u001b[A', end='', flush=True)
 
-			vertical[0] -= 1
-			curr_line = screen[vertical[0]]
-			print(f'\u001b[32m{curr_line}\u001b[0m', end = '', flush=True)
+				vertical[0] -= 1
+				curr_line = screen[vertical[0]]
+				print(f'\u001b[32m{curr_line}\u001b[0m', end = '', flush=True)
 
 
-	if e.name == 'down':
-		if vertical[0] < len(screen) - 1:
-			curr_line = screen[vertical[0]]
-			print(f'\r\u001b[0m{curr_line}', end='', flush=True)
-			print(f'\r\u001b[B', end='', flush=True)
+		if e.name == 'down':
+			if vertical[0] < len(screen) - 1:
+				curr_line = screen[vertical[0]]
+				print(f'\r\u001b[0m{curr_line}', end='', flush=True)
+				print(f'\r\u001b[B', end='', flush=True)
 
-			vertical[0] += 1
+				vertical[0] += 1
+				curr_line = screen[vertical[0]]
+				print(f'\u001b[32m{curr_line}\u001b[0m', end = '', flush=True)
+		
+		
+		if e.name == 'enter':
 			curr_line = screen[vertical[0]]
-			print(f'\u001b[32m{curr_line}\u001b[0m', end = '', flush=True)
-	
-	if e.name == 'enter':
-		curr_line = screen[vertical[0]]
-		line = curr_line
-		print(f'\r\u001b[33m{curr_line}\u001b[0m', end='', flush=True)
-		down_num = len(screen) - vertical[0]
-		print(f'\r\u001b[{down_num}B{curr_line.strip()}', end='', flush=True)
+			print(f'\r\u001b[33m{curr_line}\u001b[0m', end='', flush=True)
+			down_num = len(screen) - vertical[0] 
+			print(f'\r\u001b[{down_num}B{curr_line.strip()}', end='', flush=True)
+			line[0] = curr_line.strip()
+			horizontal[0] = len(line[0])
+	else:
+		if e.name == 'left':
+			print(f'\u001b[D', end='', flush=True)
+			horizontal[0] -= 1
+
+		elif e.name == 'right':
+			print(f'\u001b[C', end='', flush=True)
+			horizontal[0] += 1
+		
+		elif e.name == 'up':
+			print(f'\u001b[34m{context_menu[cm_idx[0]]}\u001b[0m', end='', flush=True)	
+					
+			cm_idx[0] += 1
+			if cm_idx[0] >= len(context_menu):
+				cm_idx[0] = 0
+			context[0] = context_menu[cm_idx[0]]
+			print(f'{line[horizontal[0]:]}', end='', flush=True)
+			print(f'\u001b[{len(line)-horizontal[0]}D', end='', flush=True)
 
 with open(os.path.join(os.path.dirname(__file__), 'test.json'), 'r') as f:
 	file_content = json.load(f)
@@ -42,11 +63,15 @@ vertical = [0]
 screen = []
 curr_line = ''
 bracket_check = False
+string_check = False
 for index,char in enumerate(json.dumps(file_content, indent=4)):
 	if char == '[':
 		bracket_check = True
 	elif char == ']':
 		bracket_check = False
+
+	if char == '"':
+		string_check = not string_check
 
 	if char == '\n' and not bracket_check:
 		screen.append(curr_line)
@@ -54,7 +79,7 @@ for index,char in enumerate(json.dumps(file_content, indent=4)):
 		continue
 
 	if bracket_check:
-		if char == ' ':
+		if char == ' ' and not string_check:
 			continue
 
 	curr_line += char if char != '\n' else ''
@@ -68,8 +93,12 @@ for i in range(len(screen)):
 		print(screen[i], end = '', flush=True)
 
 vertical[0] = len(screen) - 1
+horizontal = [0]
 
-line = ''
+line = ['']
+context_menu = ['[]', '\{\}', '""', ":", '()']
+context = ['']
+cm_idx = [0]
 
-keyboard.on_press(lambda e: update_variable(e, vertical, screen))
+keyboard.on_press(lambda e: update_variable(e, vertical, screen, line, context_menu,context, horizontal, cm_idx))
 keyboard.wait()
