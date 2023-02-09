6090fab3a14f6249c999ad05ad2a4a31cf01e1d7
D32\zak <spseplze>
2023-02-09 13:14:21 +0100

    add logic with getHit in player and calc outside

--- python/players/player_scripts/attack.py
+++ python/players/player_scripts/attack.py
@@ -1,4 +1,4 @@
 unique_pid = ''
 
 def attack(p1, p2, attack_info):
-	
+	print('huh?')
--- python/players/players.py
+++ python/players/players.py
@@ -1,8 +1,6 @@
 import json
 import random
 
-#This is the first general file
-
 class Player:
 	
 	def __init__(self, PlayerFile):
@@ -18,24 +16,26 @@ class Player:
 		if self.pid == '':
 			self.pid = ''.join(random.choice('0123456789abcdef'), 8) # u32
 		
-		self.player_info = PlayerData["player_info"]
+		self.info = PlayerData["info"]
+		self.inventory = PlayerData["inventory"]
 
 		
 	def getVar(self, var):
-		return self.player_info[var]
+		return self.info[var]
 
 	def setVar(self, var):
-		self.player_info[var] = var
+		self.info[var] = var
 	
 	def getHit(self, damage):
-		self.player_info["HP"] -= damage
+		self.info["HP"] -= damage
 
 	def save(self):
 
 		PlayerData = {}
 		PlayerData["ID"] = self.pid
 		PlayerData["type"] = "player"
-		PlayerData["player_info"] = self.player_info
+		PlayerData["player_info"] = self.info
+		PlayerData["inventory"] = self.inventory
 		
 		with open(self.pid + '.json', 'w') as f:
 			json.dump(PlayerData, f, indent=4)
