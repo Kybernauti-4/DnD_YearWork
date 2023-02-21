d5d34194517780b858ebefd3838478a5a23d231a
Patrik Holub <23bulohp@gmail.com>
2023-02-21 18:37:16 +0100

    attack script works!

--- python/players/attack.py
+++ python/players/attack.py
@@ -1,9 +1,11 @@
+import players
+
 unique_pid = 'global'
 
-def attack(p1, p2, bufs):
+def attack(p1, p2, bufs = None):
 	# p1 is the attacker
 	weapon = None
-	for item in p1.equipped:
+	for item in p1.equiped:
 		if item['type'].split('-')[0] == 'weapon':
 			weapon = item
 			break
@@ -14,7 +16,7 @@ def attack(p1, p2, bufs):
 		damage = weapon['damage']
 	
 	if weapon['type'].split('-')[1] != 'magic':
-		for item in p2.equipped:
+		for item in p2.equiped:
 			if item['type'].split('-')[0] == 'armor':
 				damage -= item['Defense']
 	
--- python/players/playerTest.py
+++ python/players/playerTest.py
@@ -1,6 +1,6 @@
 import os
 import players
-import player_scripts.attack
+import attack
 
 
 p1_file = os.path.join(os.path.dirname(__file__), 'player_f436c19c.json')
@@ -9,6 +9,8 @@ p2_file = os.path.join(os.path.dirname(__file__), 'player_68acf1c9.json')
 
 p1 = players.Player(p1_file)
 p2 = players.Player(p2_file)
+
+attack.attack(p1, p2)
 
 p1.usedItem(p1.equiped[0])
 p1.save()
