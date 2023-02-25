dabdacc68e7e809e111f83aea67ca31feab2827b
Patrik Holub <23bulohp@gmail.com>
2023-02-25 22:41:27 +0100

    my head hurts

---
+++ python/story/events_scripts/excludeSP.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+def excludeSP(storyStack, SP_ID):
+	
+	for root, directories, files in os.walk('story'):
+		# Iterate over the files in the current folder
+		for filename in files:
+			# Check if the file is named "storypart.txt"
+			if filename == "storypart.json":
+				ID = json.load(open(os.path.join(root, filename), 'r'))['include']
+				if ID == SP_ID:
+					storyStack.pop(storyStack.index(os.path.join(root, filename)))
+					return
---
+++ python/story/events_scripts/includeSP.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+def includeSP(storyStack, story_index, SP_ID):
+
+	for root, directories, files in os.walk('story'):
+		# Iterate over the files in the current folder
+		for filename in files:
+			# Check if the file is named "storypart.txt"
+			if filename == "storypart.json":
+				ID = json.load(open(os.path.join(root, filename), 'r'))['include']
+				if ID == SP_ID:
+					storyStack.insert(story_index, os.path.join(root, filename))
+					return
