0ce4b3d5aa4b8b91244fae6934d2ff8951211dac
Patrik Holub <23bulohp@gmail.com>
2023-03-20 15:58:00 +0100

    tec + docstring

--- python/gameBody.py
+++ python/gameBody.py
@@ -22,22 +22,58 @@ valueStack = stack.listStack() #* The global stack that will be used
 storyStack = []
 
 
-#* import loop
-scripts_path = fileHandler.read('scriptlocation.json')
+#* import section
+# create a list of locations of the scripts
+scripts_path = {}
+root_story_path = {}
+# first try to find the story folder
+i = 1 # used if there are multiple stories
+for root, directories, files in os.walk(os.getcwd()):
+	for filename in files:
+		if filename == '_story.txt':
+			if root_story_path == '':
+				root_story_path[i] = root
+				i += 1
 
-event_scripts_list = []
+# let the user choose if there are multiple stories
+if len(root_story_path) > 1:
+	print('Multiple stories found, please choose one:')
+	for index, path in root_story_path.items():
+		print('{}) {}'.format(index, path))
+	try:
+		choice = int(input('Choice: '))
+	except:
+		print('Invalid choice, please use a number next time')
+		exit()
+	
+	root_story_path = root_story_path[choice]
+else:
+	root_story_path = root_story_path[1]
 
+# then find the scripts
+i = 1 # the index of the script location, recreating original file structure which was {1:'script_location_1', 2:'script_location_2', ...}
+for root, directories, files in os.walk(root_story_path):
+	for filename in files:
+		if filename == '_scripts.txt':
+			scripts_path[i] = root
+			i += 1
+			break
+
+# preparing values for the import
+event_scripts_list = [] 
 info = {'id':{},'info':{}}
 
 for index,path in scripts_path.items():
-	import_path = os.path.join(os.getcwd(),path)
-	sys.path.insert(int(index), import_path)
+	import_path = os.path.join(os.getcwd(),path) # create the import path in relation to runtime location
+	sys.path.insert(int(index), import_path) # insert the path into the sys.path list so we can import from it
 	for event in os.listdir(import_path):
-		if os.path.isfile(os.path.join(import_path, event)) and '.py' in event: 
-			event_scripts_list.append(event.replace('.py',''))
-		if 'info.json' in event:
-			pure_info = fileHandler.read(os.path.join(import_path, event))
-			for key,value in pure_info['id'].items():
+		if os.path.isfile(os.path.join(import_path, event)) and '.py' in event: # check if it is a file and if it is a python file
+			event_scripts_list.append(event.replace('.py','')) # add the event to the list of events
+		if 'info.json' in event: # handling of the info.json file
+			pure_info = fileHandler.read(os.path.join(import_path, event)) # read the info.json file
+			# the info file has to include a id and info section
+			# nested sections are allowed in the info section
+			for key,value in pure_info['id'].items(): 
 				info['id'][key] = value
 			for info_type in pure_info['info']:
 				if info_type not in info['info']:
@@ -45,25 +81,38 @@ for index,path in scripts_path.items():
 				for value in pure_info['info'][info_type]:
 					info['info'][info_type].append(value)
 			
+# when I have the list of scripts and event that I need I can actually import them
 import_list = []
 #print(event_scripts_list)
 for event in event_scripts_list:
 	module = importlib.import_module(event)
 	import_list.append(module)
 
+# create a dictionary with the structure {event:imported_module}
 #print(import_list)
 imports = dict(zip(event_scripts_list, import_list))
 #for import_key,import_name in imports.items():
 #	print("Imported: {} => {}".format(import_key, import_name))
 
+#* end of import section
+
+#* Handler section
 def addressReplace(arguments_in):
+	"""A subsection of handle that replaces &id with the value of that id from memory
+
+	Args:
+		arguments_in (list): a list of arguments that will be passed to the function
+
+	Returns:
+		list: a list with replaced values, with the list the function can be called
+	"""
 	stack = valueStack.getValue()
 	arguments = arguments_in.copy()
 	for arg in arguments:
 		try:
-			if '&' in arg: # check if I am asking for adress
-				id = arg.replace('&','') # get the numeric adress
-				for value in stack: # search stack for that addres which is always at [1] in element
+			if '&' in arg: # check if I am asking for address
+				id = arg.replace('&','') # get the numeric address
+				for value in stack: # search stack for that address which is always at [1] in element
 					if value[1] == int(id): # if found
 						arguments[arguments.index(arg)] = value[0] # assign that value to replace the adress
 						if int(id) not in info['info']['lock']: # if the value is not locked
@@ -74,6 +123,15 @@ def addressReplace(arguments_in):
 	return arguments
 
 def handle(event_string, arguments_in):
+	"""This function is used to call events (their respective functions) and pass on arguments, 
+	when the function ends, handle will try to either save the return value to memory, or in case of logic functions,
+	it will modify the current event file to include the new events
+
+	Args:
+		event_string (str): event name, doesn't include the last part of the file name (_X where X is the index)
+		arguments_in (list): raw list of arguments as is in the event file, address replace will handle memory manipulation
+	"""
+
 	#print("Handling: {} => {}".format(event_string, arguments))
 	#input('Press enter to continue')
 	arguments = addressReplace(arguments_in)
@@ -116,10 +174,19 @@ def handle(event_string, arguments_in):
 		use_obj = obj(*arguments) # create the object and unpack the argument list into the constructor arguments
 		valueStack.append([use_obj,event_string_id]) # add the object to the stack
 
+#* End of handler section
 
-
-#* used more or less just for getting values into variables in the main loop
+#* helper functions
+#used more or less just for getting values into variables in the main loop
 def getValue(id):
+	"""You can find this function inside the stack object definition
+
+	Args:
+		id (str or int): the id of the value you want to get
+
+	Returns:
+		any: the value of the id
+	"""
 	stack = valueStack.getValue()
 	for value in stack:
 		if value[1] == int(id):
@@ -127,13 +194,15 @@ def getValue(id):
 			if int(id) not in info['info']['lock']:
 				valueStack.pop(stack.index(value))
 			return return_val
+#* End of helper functions
 
+#* Garbage collector
 def garbageCollector():
 	stack = valueStack.getValue()
 	for value in stack:
 		if int(value[1]) not in info['info']['lock']:
 			valueStack.pop(stack.index(value))
-
+#* End of garbage collector
 #* The main loop
 if __name__ == "__main__":
 	#include globals
--- python/story/events_scripts/addSP.py
+++ python/story/events_scripts/addSP.py
@@ -2,11 +2,11 @@ import os
 import json
 
 def addSP(path, storyStack, story_index, SP_ID):
-
-	for root, directories, files in os.walk('story'):
+	root_path = path.split('\\')[0]
+	for root, directories, files in os.walk(root_path):
 		# Iterate over the files in the current folder
 		for filename in files:
-			# Check if the file is named "storypart.txt"
+			# Check if the file is named "storypart.json"
 			if filename == "storypart.json":
 				ID = json.load(open(os.path.join(root, filename), 'r'))['ID']
 				if ID == SP_ID:
