a44c69f9e7f3ab177c5a8bb619c0b78233d7e954
Patrik Holub <23bulohp@gmail.com>
2023-04-05 21:05:42 +0200

    ;,{

--- python/story/player_scripts/getEnemyAlive.py
+++ python/story/player_scripts/getEnemyAlive.py
@@ -4,3 +4,9 @@ id = 'global'
 name = ''
 
 def getEnemyAlive(enemy_dict):
+	alive_list = []
+	for enemy in enemy_dict:
+		if enemy['info']['status']=='alive':
+			alive_list.append(enemy)
+
+	return alive_list
