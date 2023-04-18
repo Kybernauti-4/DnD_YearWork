044d3494e909a76827472f8171b6e66cad89bdf2
Patrik Holub <23bulohp@gmail.com>
2023-04-18 19:13:48 +0200

    test

--- python/qtest.py
+++ python/qtest.py
@@ -1,15 +1,7 @@
-from story.init.Player import Player
-from story.player_scripts.addItem import addItem
-from story.player_scripts.removeItem import removeItem
+def  add(a,b):
+    return a+b
 
-p = Player('story/players/player_00ca29e469c434dc.json')
-path = 'story\\Chapter_1\\Encounter_1\\Scene_1'
-print(addItem(path, p, 'bad_sword'))
-removeID = ''
-for item in p.inventory:
-	if item['name'] == 'Bad Sword':
-		removeID = item['ID']
-		break
-
-print(removeItem(p, removeID))
-print(p.inventory)
+if __name__=="__main__":
+    print(add(2,3))
+    print(add(4,5))
+    
